8a669f5c47c6fe89c42f4dd4657d29eb680b5e9a
mondayscool <76455195+mondayscool@users.noreply.github.com>
2022-02-21 11:18:17 -0800

    built landing page, closes #4

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+<html>
+    <head>
+        <title>RiskWyse : Financial Learning and Consulting</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+
+    <body>
+        <div class="container">
+            <h1 class="text-center text-primary mb-3">RiskWyse</h1>
+
+            <div class="container w-25">
+                <a class="btn btn-primary p-4 d-block mb-3" href="learning_home.php">
+                    Individual Learning
+                </a>
+                <a class="btn btn-secondary p-4 d-block mb-3" href="consulting_home.php">
+                    Corporate Consulting
+                </a>
+            </div>
+        </div>
+    </body>
+</html>
