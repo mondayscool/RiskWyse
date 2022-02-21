638d035b8775a867dff09d03e7c8b8b8be7a2fa3
mondayscool <76455195+mondayscool@users.noreply.github.com>
2022-02-21 14:41:40 -0800

    built course view page, closes #15

--- course_view.php
+++ course_view.php
@@ -1,21 +1,31 @@
 <?php
     session_start();
 
+    $course_id = filter_input(INPUT_GET, 'course_id');
+
     $user_id = $_SESSION["user_id"];
     $f_name = $_SESSION["f_name"];
     $l_name = $_SESSION["l_name"];
 
     $connection = mysqli_connect("69.172.204.200", "mondaysc_riskwyse_user", "Spaceballs1!", "mondaysc_riskwyse");
-    $user_courses_query = "SELECT c.id, c.name, c.description FROM user_courses uc, courses c WHERE uc.course_ID = c.ID AND uc.user_ID=${user_id};";
-    $course_result = mysqli_query($connection, $user_courses_query) or die(mysqli_error($connection));
+
+    $course_info_query = "SELECT * FROM courses WHERE ID=${course_id};";
+    $course_info_result = mysqli_query($connection, $course_info_query) or die(mysqli_error($connection));
+
+    $course_info_array = mysqli_fetch_array($course_info_result);
+    $course_name = $course_info_array["name"];
+    $course_description = $course_info_array["description"];
+
+    $course_videos_query = "SELECT * FROM videos WHERE course_id=${course_id}; ";
+    $videos_result = mysqli_query($connection, $course_videos_query) or die(mysqli_error($connection));
 
     $table = "";
-    if(mysqli_num_rows($course_result) > 0) {
-        while($row = mysqli_fetch_array($course_result)) {
+    if(mysqli_num_rows($videos_result) > 0) {
+        while($row = mysqli_fetch_array($videos_result)) {
             $table .= "<tr>";
-            $table .= "<td>${row["name"]}</td>";
+            $table .= "<td>${row["title"]}</td>";
             $table .= "<td>${row["description"]}</td>";
-            $table .= "<td><a class='btn btn-primary' href='course_view.php?course_id=${row["ID"]}'>WATCH NOW</a></td>";
+            $table .= "<td><a class='btn btn-primary' href='video_view.php?video_id=${row["ID"]}'>WATCH NOW</a></td>";
             $table .= "</tr>";
         }
     }
@@ -50,7 +60,10 @@
         </nav>
 
         <div class="container w-50">
-            Welcome, <?php echo $f_name; ?>
+            <h1 class="text-primary"><?php echo $course_name; ?></h1>
+            <div class="container">
+                <?php echo $course_description; ?>
+            </div>
 
             <table class="table table-hover">
                 <?php echo $table; ?>
@@ -58,4 +71,3 @@
         </div>
     </body>
 </html>
-
